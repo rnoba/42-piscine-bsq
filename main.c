5c5997890b3a7786225f32656db7366d4d623dfc
Rafael Nogueira Barros <rnogueir@c2r4p6.42sp.org.br>
2023-09-05 18:40:44 -0300

    Revert "main"
    This reverts commit 17c8e1109b569c1bd59d44f98bb51424840215b1.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ovasconc <otaviocavasc2@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 11:25:13 by rnogueir          #+#    #+#             */
-/*   Updated: 2023/09/05 18:37:55 by rnogueir         ###   ########.org.br   */
+/*   Updated: 2023/09/05 17:50:47 by ovasconc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,9 @@
 #include "validators.h"
 #include "map.h"
 
-int main(int argc, char **argv)
+int main(void)
 {
-	char *buffer;
-	char **map;
-	int fd;
-	
-	buffer = malloc(sizeof(char) * BUFFER_SIZE);
-	if(argc > 1)
-	{
-		fd = ft_readfile(argv[1]);	
-		ft_read(fd, buffer);
-	} else {
-		ft_read(0, buffer);
-	}
-	map = ft_split(buffer, '\n');
-	if(ft_is_valid_map(map))
-		ft_solve(map);
+	char *s = "1545.ox\n....\n..o.\n....\noooo";
+
+	ft_is_valid_map(ft_split(s, '\n'));
 }
